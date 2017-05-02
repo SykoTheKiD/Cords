27a2f1ee666f8bd23c6e0e8f2027840d1f51d937
Jay SyKo <jay@jaysyko.com>
2017-05-01 21:42:23 -0400

    Added working check

--- main.py
+++ main.py
@@ -1,84 +1,52 @@
 # !/usr/python3
 
-
+PIECES = {'X' : 1, 'O': 2}
 
 class Board:
 
 	def __init__(self, size):
 		self.terminal = '$'
 		self.size = size
-		self.board = []
-
+		self.grid = []
+		PIECES[self.terminal] = -1
 		for i in range(size):
 			tmp = ['$' for j in range(0, size)]
-			self.board.append(tmp)
+			self.grid.append(tmp)
 
 	def add_move(self, piece, x, y):
 		x -=1
 		y -=1
 		if(x < self.size and y < self.size):
-			pos = self.board[x][y]
+			pos = self.grid[x][y]
 			if(pos == self.terminal):
-				self.board[x][y] = piece
+				self.grid[x][y] = piece
 
 	def print_board(self):
-		print(self.board)
+		print(self.grid)
 		for i in range (0, self.size):
 			for j in range (0, self.size):
-				print(self.board[i][j])
+				print(self.grid[i][j])
 
-# bruh
+def winning_piece(*lists):
+	for each_list in lists:
+		if(len(each_list) > 0):
+			reduced = list(set(each_list))
+			if(each_list[0] == '$' and len(reduced) != 1):
+				break
+			return reduced[0]
+
 def check_board(board):
+	grid = board.grid
 	board_size = board.size
-	current_character = None
-
-	for i in range(0, board_size):
-		char = board[0][i]
-		if(not current_character):
-			current_character = char
-
-		if(current_character != char):
-			break
-
-		return current_character
-
-	for i in range(0, board_size):
-		board[board_size][i]
-
-		if(not current_character):
-			current_character = char
-
-		if(current_character != char):
-			break
-
-		return current_character
-
-	for i in range(0, board_size):
-		board[i][0]
-
-		if(not current_character):
-			current_character = char
-
-		if(current_character != char):
-			break
-
-		return current_character
-
-	for i in range(0, board_size):
-		board[i][board_size]
-
-		if(not current_character):
-			current_character = char
-
-		if(current_character != char):
-			break
-
-		return current_character
-
+	top  = [grid[0][i] for i in range (0, board_size)]
+	left = [grid[i][0] for i in range (0, board_size)]
+	left_diagonal = [grid[i][i] for i in range (0, board_size)]
+	right_diagonal = [grid[i][board_size -1 - i] for i in range (0, board_size)]
+	return winning_piece(top, left, left_diagonal, right_diagonal)
 
 def main():
 	board = Board(3)
-	board.print_board()
+	check_board(board)
 
 
 if __name__ == "__main__":
